4ea6b3f6940e53f3e38fe4be47ca280df53fce17
Volodymyr892 <sevcukvolodimir896@gmail.com>
2025-01-13 12:33:12 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import {configureStore} from "@reduxjs/toolkit"
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist"
+import persistStore from "redux-persist/es/persistStore"
+
+
+export const store = configureStore({
+    reducer: {
+
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+    }),
+})
+
+export const persistor = persistStore(store);
